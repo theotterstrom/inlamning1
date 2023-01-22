dff3bbbc196eff71ed4ddecb681ae17b1baee78d
Lukas Otterström <lukot923@student.liu.se>
2023-01-22 20:42:13 +0100

    Add project files.

---
+++ Inlämning 1/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+
+namespace Inlämning_1
+{
+    public class AnimalCollection
+    {
+        public ObservableCollection<Animal> animalList;
+
+        public void createAnimal(ComboBox regBox, Boolean isDog, String descBox, String raceBox)
+        {
+            Animal tempan = new Animal()
+            {
+                regNr = regBox.Text,
+                race = raceBox,
+                description = descBox,
+                isDog = isDog
+
+            };
+
+            /*ComboBoxItem tempItem = new ComboBoxItem();
+            tempItem.Content = tempan.regNr;
+            regBox.Items.Add(tempItem);*/
+
+            animalList.Add(tempan);
+            
+            regBox.DataContext = animalList;
+            regBox.DisplayMemberPath = "name";
+        }
+       
+    }
+
+        public class Animal : AnimalCollection
+        {
+            public String regNr { get; set; }
+            public String race { get; set; }
+            public String description { get; set; }
+            public bool isDog { get; set; }
+        }
+    }
+
---
+++ Inlämning 1/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Inlämning_1
+{
+    public partial class MainWindow : Window
+    {
+
+        public AnimalCollection animalCol = new AnimalCollection();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ComboBox cmb = (ComboBox)typeComboBox;
+            ComboBoxItem dogOrCat = (ComboBoxItem)cmb.SelectedItem;
+            bool isDog = true;
+            if (dogOrCat.Content.ToString() == "Cat")
+            {
+                isDog = false;
+            }
+            animalCol.createAnimal(regBox, isDog, descBox.Text, raceBox.Text);
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> strList = new List<String>();
+            var animalList = animalCol.animalList;
+
+            System.Windows.Forms.SaveFileDialog saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            saveFileDialog1.Filter = "Text (*.csv)|*.csv";
+            saveFileDialog1.FileName = "Animallist.csv";
+            saveFileDialog1.ShowDialog();
+            StreamWriter writer = new StreamWriter(saveFileDialog1.OpenFile());
+            for (int i = 0; i < animalList.Count; i++)
+            {
+                String dogOrCat = "cat";
+                if (animalList[i].isDog)
+                {
+                    dogOrCat = "Dog";
+                }
+                String tempstr = "regNr:" + animalList[i].regNr + ", race:" + animalList[i].race + ", description:" + animalList[i].description + ", type:" + dogOrCat;
+                writer.WriteLine(tempstr);
+            }
+            writer.Dispose();
+            writer.Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
